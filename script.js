ae5069a7d4e21b45edb7366465303026dc9286a0
Robert DeVore <deviodigital@gmail.com>
2024-11-12 01:33:04 -0500

    :ok_hand: IMPROVE: General code cleanup

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-jQuery(document).ready(function ($) {
+jQuery(function ($) {
     $('.view-details-button').on('click', function (e) {
         e.preventDefault();
 
         let blockName = $(this).data('block');
-        let postsData = $(this).data('posts'); // Directly access data without JSON.parse()
+        let postsData = $(this).data('posts');
         let modalBody = $('#block-usage-modal-body');
 
-        // Clear the modal content from any previous usage
+        // Clear the modal content from any previous usage.
         modalBody.empty();
 
-        // Display a summary message at the top
+        // Display a summary message at the top.
         let usageCount = postsData.length;
         modalBody.append('<p><strong>The ' + blockName + ' block was found ' + usageCount + ' times in the following content:</strong></p>');
 
-        // Filter unique URLs to avoid duplicates
+        // Filter unique URLs to avoid duplicates.
         let uniquePosts = [];
         let uniqueUrls = new Set();
 
@@ -24,7 +24,7 @@ jQuery(document).ready(function ($) {
             }
         });
 
-        // Display each unique post
+        // Display each unique post.
         if (uniquePosts.length > 0) {
             uniquePosts.forEach(post => {
                 modalBody.append('<p><a href="' + post.url + '" target="_blank">' + post.title + '</a></p>');
@@ -33,16 +33,16 @@ jQuery(document).ready(function ($) {
             modalBody.append('<p>No posts found for this block.</p>');
         }
 
-        // Show the modal
+        // Show the modal.
         $('#block-usage-modal').fadeIn();
     });
 
-    // Close the modal when the close button is clicked
+    // Close the modal when the close button is clicked.
     $('.block-usage-close').on('click', function () {
         $('#block-usage-modal').fadeOut();
     });
 
-    // Close the modal when clicking outside of the modal content
+    // Close the modal when clicking outside of the modal content.
     $(window).on('click', function (e) {
         if ($(e.target).is('#block-usage-modal')) {
             $('#block-usage-modal').fadeOut();
